d396df3ae2db161a69b9bfe79f7e06d1b621710b
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-04-16 11:01:30 +0100

    try with theme null

--- src/DocumenterMermaid.jl
+++ src/DocumenterMermaid.jl
@@ -33,7 +33,7 @@ function Documenter.HTMLWriter.domify(dctx::DCtx, node::Node, ::MermaidScriptBlo
     import mermaid from '$MERMAID';
     mermaid.initialize({
         startOnLoad: true,
-        theme: "base",
+        theme: null,
         deterministicIds: true,
         flowchart: { htmlLabels: true },
     });
